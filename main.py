3fe11fd8772c131301588f1960d3026bcc5b6cf1
henkling <hen@jit.io>
2022-06-30 15:50:58 +0300

    .

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 if __name__ == '__main__':
     print('hello world2')
+    d = subprocess.check_output(f"nslookup2 {domain}", shell=True, encoding='UTF-8')
+    d = subprocess.check_output(f"nslookup2 {domain}", shell=True, encoding='UTF-8')
+
+    d = subprocess.check_output(f"nslookup2 {domain}", shell=True, encoding='UTF-8')
+
+    d = subprocess.check_output(f"nslookup2 {domain}", shell=True, encoding='UTF-8')
+
+    d = subprocess.check_output(f"nslookup2 {domain}", shell=True, encoding='UTF-8')
+    d = subprocess.check_output(f"nslookup2 {domain}", shell=True, encoding='UTF-8')
+
+    d = subprocess.check_output(f"nslookup2 {domain}", shell=True, encoding='UTF-8')
+    d = subprocess.check_output(f"nslookup2 {domain}", shell=True, encoding='UTF-8')
+    d = subprocess.check_output(f"nslookup2 {domain}", shell=True, encoding='UTF-8')
