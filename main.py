4cc390a4399f29beef31ded9ceef380e49c76b18
henkling <105640757+henkling@users.noreply.github.com>
2022-07-03 11:34:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,4 @@
 if __name__ == '__main__':
     print('hello world2')
-    d = subprocess.check_output(f"nslookup2 {domain}", shell=True, encoding='UTF-8')
-    d = subprocess.check_output(f"nslookup2 {domain}", shell=True, encoding='UTF-8')
-
-    d = subprocess.check_output(f"nslookup2 {domain}", shell=True, encoding='UTF-8')
-
-    d = subprocess.check_output(f"nslookup2 {domain}", shell=True, encoding='UTF-8')
-
-    d = subprocess.check_output(f"nslookup2 {domain}", shell=True, encoding='UTF-8')
-    d = subprocess.check_output(f"nslookup2 {domain}", shell=True, encoding='UTF-8')
-
-    d = subprocess.check_output(f"nslookup2 {domain}", shell=True, encoding='UTF-8')
-    d = subprocess.check_output(f"nslookup2 {domain}", shell=True, encoding='UTF-8')
-    d = subprocess.check_output(f"nslookup2 {domain}", shell=True, encoding='UTF-8')
+    x = 4
+    y = 4
