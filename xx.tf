e0b236cd358c4a9b021c9e100e26fd467bbb3d18
henkling <105640757+henkling@users.noreply.github.com>
2022-08-14 12:26:34 +0300

    Update xx.tf
    Co-authored-by: jit-ci-birds[bot] <96962445+jit-ci-birds[bot]@users.noreply.github.com>

--- xx.tf
+++ xx.tf
@@ -1,4 +1,5 @@
 resource "aws_cloudtrail" "positive1" {
+  kms_key_id = "arn:aws:kms:<Your KMS Key>"
   name                          = "npositive_1"
   s3_bucket_name                = "bucketlog_1"
 }
